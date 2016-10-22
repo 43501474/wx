774e14a2284ab154c62555169a784f682ecee912
cph <phchang@qq.com>
2016-10-22 09:33:21 +0800

    index和wx接口调通

--- wxapp/views.py
+++ wxapp/views.py
@@ -6,4 +6,8 @@ from django.http.response import HttpResponse
 
 def wx(req):
     '''微信接入验证'''
-    return HttpResponse(req.GET("echostr",  ""), content_type="text/plain")
+    return HttpResponse(req.GET.get("echostr",  ""), content_type="text/plain")
+
+
+def index(req):
+    return HttpResponse("Hello, world!")
