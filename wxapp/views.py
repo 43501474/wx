d14149e76496f9836ab958fdf0b54fbc8bb81fd0
patrick.chang <patrick.chang@chebaike100.com>
2017-01-03 10:27:54 +0800

    将接入验证和消息处理合并为一个view，因为微信智能配置要给url。

--- wxapp/views.py
+++ wxapp/views.py
@@ -7,9 +7,21 @@ from django.http.response import HttpResponse
 
 
 def wx(req):
-    '''微信接入验证'''
-    return HttpResponse(req.GET.get("echostr",  ""), content_type="text/plain")
-
+    '''微信消息处理'''
+    if 'echostr' in req.GET:    # 处理接入验证
+        return HttpResponse(req.GET.get("echostr",  ""), content_type="text/plain")
+    else:   # 处理消息 
+        from .reqparser import text_req
+        from .rspbuilder.textbuilder import TextMsg
+        msg = text_req.parse(req.body)
+    
+        if isinstance(msg, text_req.Msg) and msg.MsgType == 'text':
+            toUser = msg.FromUserName
+            fromUser = msg.ToUserName
+            content = "test"
+            replyMsg = TextMsg(toUser, fromUser, content)
+    
+            return HttpResponse(str(replyMsg), content_type="text/xml")
 
 def index(req):
     return HttpResponse("Hello, world!")
